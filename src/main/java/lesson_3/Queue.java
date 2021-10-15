9d1bee4a27a71e27433d4904968c75fb6dfecb30
MoskvichevViktor <beldetal@yandex.ru>
2021-10-15 23:18:11 +0300

    finish

---
+++ src/main/java/lesson_3/Queue.java
@@ -0,0 +1,20 @@
+package lesson_3;
+
+public interface Queue<E> {
+
+    boolean insert(E value);
+
+    E remove();
+
+    E peekFront();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
+
